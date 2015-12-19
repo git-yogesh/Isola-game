29e24de1e493ec566df5950e2c422906aa4a6b1d
shreyas-srp <shreyas.srp@gmail.com>
2015-12-19 13:44:02 +0530

    splash modified

--- app/src/main/java/com/algosee/prerk/isola/GameFragment.java
+++ app/src/main/java/com/algosee/prerk/isola/GameFragment.java
@@ -23,6 +23,8 @@ public class GameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        gameLogic game1 = new gameLogic();
+
         return inflater.inflate(R.layout.fragment_game, container, false);
     }
 
